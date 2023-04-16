812fffaa694d7bb67627b3c30dee52e2e9b9de56
Tu nombre de usuario <juholguinm@unal.edu.co>
2023-04-15 22:34:30 -0500

    LAB 5n

--- pregunta.py
+++ pregunta.py
@@ -12,35 +12,25 @@ import pandas as pd
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
-    df.reset_index(inplace=True,drop=True)
+    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";").drop(columns=['Unnamed: 0'])
     
-    #Fecha
-    df['fecha_de_beneficio']= pd.to_datetime (df['fecha_de_beneficio'], dayfirst=True)
     
-    df.dropna(axis='index', inplace=True)
+    df.dropna(inplace=True)
     df.drop_duplicates(inplace=True)
-    
-    #Minusculas
-    df['sexo'] = df['sexo'].str.lower().astype(str).str.strip()
-    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower().astype(str)
-    df['idea_negocio'] = df['idea_negocio'].str.lower().astype(str)
-    df['barrio'] = df['barrio'].str.lower().astype(str)
-    df['línea_credito'] = df['línea_credito'].str.lower().astype(str)
-    
-    #Blancos, ocultos
-    df['idea_negocio'] = df['idea_negocio'].str.replace('_',' ').str.replace('-',' ').str.strip()
-    df['barrio'] = df['barrio'].str.replace('_',' ').str.replace('-',' ').str.strip()
-    df['línea_credito'] = df['línea_credito'].str.replace('_',' ').str.replace('-',' ').str.strip()
-    
-    #Montos
-    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',','').str.replace('$','',regex=False).str.replace(' ','').str.strip().astype(float)
-    
-    #Otras
-    df['línea_credito'] = df['línea_credito'].str.replace('soli daria','solidaria')
-    df['idea_negocio'] = df['idea_negocio'].str.replace('[^a-zA-Z0-9 \n\.]',' ',regex=True).str.replace('organizaci n','organizacion').str.replace('pa alera','pañalera')
-    df['barrio'] = df['barrio'].str.replace('[^a-zA-Z0-9 \n\.]',' ',regex=True).str.replace('bel n','belen').str.replace('san jos  de la monta a','san jose de la montana').str.replace('antonio nari  o','antonio narino').str.replace('antonio nari o','antonio narino')
-    
-    df.drop_duplicates(inplace=True)
+
+    df["sexo"] = df.sexo.str.lower().astype(str).str.strip()
+    df["tipo_de_emprendimiento"] = df.tipo_de_emprendimiento.str.lower().astype(str).str.strip()
+    df["idea_negocio"] = df.idea_negocio.str.replace("-"," ", regex=True).str.replace("_"," ", regex=True).str.lower()
+    df["barrio"] = df.barrio.str.replace("_","-", regex=True).str.replace("-"," ", regex=True).str.lower()
+    df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"], dayfirst=True)
+    df['monto_del_credito'] = df['monto_del_credito'].map(lambda x: x.lstrip('$'))
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',', '')
+    df.monto_del_credito = pd.to_numeric(df.monto_del_credito, errors='coerce')
+    df["línea_credito"] = df.línea_credito.str.replace("-"," ")
+    df["línea_credito"] = df.línea_credito.str.replace("_"," ")
+    df["línea_credito"] = df.línea_credito.str.lower()
+
+    df = df.drop_duplicates().dropna()
     
     return df
